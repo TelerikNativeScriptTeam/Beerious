b0c52f52a89c8f014f7357a5f40ea73ad4be497c
Bianca Hinova <bianca.hinova@gmail.com>
2016-02-19 14:49:06 +0200

    .

--- Beerious/app/views/splash/splash.js
+++ Beerious/app/views/splash/splash.js
@@ -1,9 +1,16 @@
 var frameModule = require("ui/frame");
+var ImageModule = require("ui/image");
 frameModule.Frame.defaultTransition = { name: "slideTop" };
 
 exports.loaded = function (argument) {
 	var page = argument.object;
 	var image = page.getViewById("imageSplash");
+	if(page.ios) {
+		//image.imageSource = "res://Default";
+	}
+	else {
+		//image.imageSource = "res://splashscreen.9";
+	}
 	image.animate({
 		opacity: 1,
 		duration: 4000
